12276ea6b0b4bd967d550143d63c67541abe33ec
wadaaaa <lopatinski@gmail.com>
2018-02-18 11:57:03 +0200

    first version api

---
+++ db.py
@@ -0,0 +1,21 @@
+PRODUCTS = [
+    {
+        "title": "Apple Iphone X",
+        "price_rub": 86080,
+        "product_image": "/images/products/iphonex.png",
+        "in_store": True,
+    },
+    {
+        "title": "Apple MacBook Pro 13",
+        "price_rub": 87080,
+        "product_image": "/images/products/macbookpro13.png",
+        "in_store": True,
+    },
+    {
+        "title": "Apple MacBook Pro 15",
+        "price_rub": 89080,
+        "product_image": "/images/products/macbookpro15.png",
+        "in_store": False,
+
+    }
+]
